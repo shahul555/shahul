6460e50d57a526cb40324d0c366d7b076cd829cc
shahul555 <42229583+shahul555@users.noreply.github.com>
2018-08-29 09:47:28 +0530

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Leap
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	     Scanner sc=new Scanner(System.in);
+	     int leap=sc.nextInt();
+	     if(leap%400==0)
+	        System.out.println("yes");
+	       else
+	          System.out.println("no");
+	}
+}
